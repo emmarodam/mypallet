ca2623f4906cdb0fb5d87cd4cfa177cb27fe15ac
Emma Rodam <emmarodam@gmail.com>
2025-01-30 16:40:04 +0100

    Docker + Laravel works

---
+++ mypallet/resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myPallet</title>
+</head>
+<body>
+
+<main>
+
+    <div class="">
+        <ul>
+            <li>Trucks</li>
+            <li>Trailers</li>
+        </ul>
+    </div>    
+        <p>Search</p>
+    
+        <h2>Trucks</h2>
+    
+        <h2>Trailers</h2>
+        
+</main>
+
+</body>
+</html>
